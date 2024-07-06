34d706d852b7b5b617ccb317ee8d1e56d0c98985
nniemier <nadianiemier@gmail.com>
2024-07-06 18:47:45 +0200

    Google sign in has been added with Firebase

--- ChatApp/screens/LoginScreen.js
+++ ChatApp/screens/LoginScreen.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { Button } from 'react-native';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 
-function LoginScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          title="Go to ChatRooms"
-          onPress={() => navigation.navigate('ChatRooms')}
-        />
-      </View>
-    );
-  }
+GoogleSignin.configure({
+  webClientId: '1013134024517-ohcvfgpqqgcuplj7vr13h8s7lr9behat.apps.googleusercontent.com',
+});
+
+function LoginScreen() {
+  return (
+    <Button
+      title="Google Sign-In"
+      onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+    />
+  );
+}
+
+async function onGoogleButtonPress() {
+  // Check if your device supports Google Play
+  await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+  // Get the users ID token
+  const { idToken } = await GoogleSignin.signIn();
+
+  // Create a Google credential with the token
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+  // Sign-in the user with the credential
+  return auth().signInWithCredential(googleCredential);
+}
 
 export default LoginScreen;
