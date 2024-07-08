aa17f2d140f55b6c0b2f28a1491abf4574820922
nniemier <nadianiemier@gmail.com>
2024-07-08 15:35:17 +0200

    Facebook sign in has been added and the button has been costumized with vector icons

--- ChatApp/screens/ChatRooms.js
+++ ChatApp/screens/ChatRooms.js
@@ -1,8 +1,59 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, FlatList } from 'react-native';
 import auth from '@react-native-firebase/auth';
+//Import for the NoSql database Firestore
+import firestore from '@react-native-firebase/firestore'; 
+
+//Store a list of chat room documents within a "ChatRooms" collection 
+//const chatRoomsCollection = firestore().collection('ChatRooms');
 
 function ChatRooms({ navigation }) {
+  /* const [loading, setLoading] = useState(true); // Set loading to true on component mount
+  const [chatRooms, setChatRooms] = useState([]); // Initial empty array of users
+
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('chatRoomsCollection')
+      .onSnapshot(querySnapshot => {
+        const chatRooms = [];
+  
+        querySnapshot.forEach(documentSnapshot => {
+          chatRooms.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+  
+        setUsers(chatRooms);
+        setLoading(false);
+      });
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  return (
+    <FlatList
+      data={chatRooms}
+      renderItem={({ item }) => (
+        <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Room Name: {item.roomName}</Text>
+          <Text>Room Description: {item.roomDescription}</Text>
+
+          <Button
+          title="Go to Specific room"
+          onPress={() => navigation.navigate('SpecificRoom')}
+        />
+        <Button title="Sign out" onPress={() => auth().signOut().then(() => console.log('User signed out!'))}/>
+        </View>
+      )}
+    />
+  );
+ */
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Button
--- ChatApp/screens/LoginScreen.js
+++ ChatApp/screens/LoginScreen.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { StyleSheet, Button } from 'react-native';
+import { StyleSheet, Button, Alert, View } from 'react-native';
 import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
+import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 GoogleSignin.configure({
   webClientId: '1013134024517-ohcvfgpqqgcuplj7vr13h8s7lr9behat.apps.googleusercontent.com',
 });
 
-function LoginScreen({navigation}) {
+function LoginScreen() {
   return (
-    <GoogleSigninButton
-  size={GoogleSigninButton.Size.Wide}
-  color={GoogleSigninButton.Color.Light}
-  onPress={() => {
-    onGoogleButtonPress().then(() => navigation.navigate('ChatRooms'))
-  }}
-/>
+    <View style={styles.container}>
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Light}
+        onPress={onGoogleButtonPress}
+      />
+      <Icon.Button name="facebook"backgroundColor="#3b5998" 
+      onPress={onFacebookButtonPress}> 
+      Sign in with Facebook
+      </Icon.Button>
+    </View>
   );
 }
 
@@ -31,5 +37,35 @@ async function onGoogleButtonPress() {
   // Sign-in the user with the credential
   return auth().signInWithCredential(googleCredential);
 }
+
+async function onFacebookButtonPress() {
+  // Attempt login with permissions
+  const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+
+  if (result.isCancelled) {
+    throw 'User cancelled the login process';
+  }
+
+  // Once signed in, get the users AccessToken
+  const data = await AccessToken.getCurrentAccessToken();
+
+  if (!data) {
+    throw 'Something went wrong obtaining access token';
+  }
+
+  // Create a Firebase credential with the AccessToken
+  const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+
+  // Sign-in the user with the credential
+  return auth().signInWithCredential(facebookCredential);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default LoginScreen;
